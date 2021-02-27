681853f34fd28783d5b0c2bab939db9b89f26519
Gabriel Alves <g04lves@gmail.com>
2021-02-27 17:03:07 -0300

    Add router routes for live view

--- lib/nfl_rushing_web/router.ex
+++ lib/nfl_rushing_web/router.ex
@@ -18,6 +18,13 @@ defmodule NflRushingWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+
+    live "/players", PlayerLive.Index, :index
+    live "/players/new", PlayerLive.Index, :new
+    live "/players/:id/edit", PlayerLive.Index, :edit
+
+    live "/players/:id", PlayerLive.Show, :show
+    live "/players/:id/show/edit", PlayerLive.Show, :edit
   end
 
   # Other scopes may use custom stacks.
