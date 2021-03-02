e3fdd4ef9673b2c611070af9c4e36fcf2fe824ea
Gabriel Alves <g04lves@gmail.com>
2021-03-02 10:38:10 -0300

    Sort players on LiveView

--- test/nfl_rushing_web/live/player_live_test.exs
+++ test/nfl_rushing_web/live/player_live_test.exs
@@ -15,8 +15,22 @@ defmodule NflRushingWeb.PlayerLiveTest do
     test "lists all players", %{conn: conn, player: player} do
       {:ok, _index_live, html} = live(conn, Routes.player_index_path(conn, :index))
 
-      assert html =~ "Listing Players"
+      assert html =~ "Players"
       assert html =~ player.name
+    end
+
+    test "sorts players", %{conn: conn} do
+      [first_player, second_player] = 2 |> insert_list(:player) |> Enum.sort_by(& &1.name, &>=/2)
+
+      {:ok, index_live, _html} = live(conn, Routes.player_index_path(conn, :index))
+
+      html =
+        index_live
+        |> form("#sorting-form", sorting: %{order: "desc", field: "name"})
+        |> render_submit()
+
+      assert html =~ "Sorting data..."
+      assert html =~ Regex.compile!("#{first_player.name}.*#{second_player.name}")
     end
   end
 end
