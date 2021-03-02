e4e32e61253909eea1428635037ecc7465c79fef
Gabriel Alves <g04lves@gmail.com>
2021-03-02 13:29:04 -0300

    Add filtering funcionality on records context

--- test/nfl_rushing/records_test.exs
+++ test/nfl_rushing/records_test.exs
@@ -4,7 +4,7 @@ defmodule NflRushing.RecordsTest do
   import NflRushing.Factory
 
   alias Ecto.UUID
-  alias NflRushing.Query.SortingParams
+  alias NflRushing.Query.{FilteringParams, SortingParams}
   alias NflRushing.Records.Player
   alias NflRushing.{Records, Repo}
 
@@ -22,6 +22,15 @@ defmodule NflRushing.RecordsTest do
 
       assert [first_player, second_player] ==
                Records.list_players(sorting_params: %SortingParams{ordering: :asc, field: :name})
+    end
+
+    test "returns filtered players" do
+      first_player = insert(:player, name: "John")
+      _second_player = insert(:player, name: "Doe")
+
+      options = [filtering_params: %FilteringParams{contains_string_filter: first_player.name, field: :name}]
+
+      assert [first_player] == Records.list_players(options)
     end
   end
 
