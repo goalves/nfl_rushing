ffd31d12d71b890d92c42da9daec2e5e40de87fa
Gabriel Alves <g04lves@gmail.com>
2021-03-02 12:45:24 -0300

    Enhance test on Query.sort to ensure it works with asc and desc

--- test/nfl_rushing/query_test.exs
+++ test/nfl_rushing/query_test.exs
@@ -11,15 +11,17 @@ defmodule NflRushing.QueryTest do
 
   describe "sort/2" do
     test "returns a query with an order by statement" do
-      sorting_params = %SortingParams{field: :field, ordering: :asc}
-      queriable = Query.sort(TestQueriable, sorting_params)
+      assert Enum.all?([:asc, :desc], fn ordering ->
+               sorting_params = %SortingParams{field: :field, ordering: ordering}
+               queriable = Query.sort(TestQueriable, sorting_params)
 
-      expected_query =
-        ~s"#Ecto.Query<from t0 in NflRushing.QueryTest.TestQueriable, order_by: [#{sorting_params.ordering}: t0.#{
-          sorting_params.field
-        }]>"
+               expected_query =
+                 ~s"#Ecto.Query<from t0 in NflRushing.QueryTest.TestQueriable, order_by: [#{ordering}: t0.#{
+                   sorting_params.field
+                 }]>"
 
-      assert inspect(queriable) == expected_query
+               assert inspect(queriable) == expected_query
+             end)
     end
 
     test "returns the given queriable if nil argument is passed",
