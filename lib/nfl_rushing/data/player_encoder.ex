b595d0062f184bff34df541f3b6b90d6e45ee920
Gabriel Alves <g04lves@gmail.com>
2021-03-02 23:53:04 -0300

    Create a encoder for player structure to CSV

---
+++ lib/nfl_rushing/data/player_encoder.ex
@@ -0,0 +1,27 @@
+defmodule NflRushing.Data.PlayerEncoder do
+  alias NflRushing.Records.Player
+
+  @spec encode(Player.t()) :: map()
+  def encode(player = %Player{}) do
+    touchdown_identifier = if player.longest_rush_had_a_touchdown?, do: "T", else: ""
+    longest_rush_with_touchdown = "#{player.longest_rush}" <> touchdown_identifier
+
+    %{
+      "Player" => player.name,
+      "Team" => player.team,
+      "Pos" => player.position,
+      "Att/G" => player.attempts_per_game,
+      "Att" => player.rushing_attemps,
+      "Yds" => player.total_rushing_yards,
+      "Avg" => player.rushing_average_yards_per_attempt,
+      "Yds/G" => player.rushing_yards_per_game,
+      "TD" => player.rushing_touchdowns,
+      "Lng" => longest_rush_with_touchdown,
+      "1st" => player.rushing_first_downs,
+      "1st%" => player.rushing_first_down_percentage,
+      "20+" => player.rushing_20_yards_each,
+      "40+" => player.rushing_40_yards_each,
+      "FUM" => player.fumbles
+    }
+  end
+end
